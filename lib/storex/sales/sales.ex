aeacd5730d0a62601facc67497d73eabcf6e59cd
Alexey Lopatin <a1ex@Alexeys-MBP.Dlink>
2018-01-05 09:04:07 +0300

    added tests and funcs for cart

--- lib/storex/sales/sales.ex
+++ lib/storex/sales/sales.ex
@@ -2,4 +2,40 @@ defmodule Storex.Sales do
   import Ecto.Query, warn: false
   alias Storex.Repo
   alias Storex.Sales.{Cart, LineItem}
+
+  def create_cart(attrs \\ %{}) do
+    %Cart{}
+    |>Cart.changeset(attrs)
+    |>Repo.insert()
+  end
+
+  def get_cart!(id) do
+    Repo.get(Cart, id)
+  end
+
+  def add_book_to_cart(book, cart) do
+    line_item = Repo.get_by(LineItem, book_id: book.id, cart_id: cart.id)
+
+    if line_item do
+      line_item
+      |> LineItem.changeset(%{quantity: line_item.quantity + 1})
+      |> Repo.update()
+    else
+      %LineItem{book_id: book.id, cart_id: cart.id}
+      |> LineItem.changeset(%{quantity: 1})
+      |> Repo.insert()
+    end
+  end
+
+  def remove_book_from_cart(book, cart) do
+    line_item = Repo.get_by!(LineItem, book_id: book.id, cart_id: cart.id)
+
+    if line_item.quantity > 1 do
+      line_item
+      |> LineItem.changeset(%{quantity: line_item.quantity - 1})
+      |> Repo.update()
+    else
+      Repo.delete!(line_item)
+    end
+  end
 end
