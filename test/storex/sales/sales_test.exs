e6161bfaa972e0609408969a127bdd7e15c20e50
Alexey Lopatin <a1ex@Alexeys-MBP.Dlink>
2018-01-07 10:15:41 +0300

    added fixtures

--- test/storex/sales/sales_test.exs
+++ test/storex/sales/sales_test.exs
@@ -2,6 +2,27 @@ defmodule Storex.SalesTest do
   use Storex.DataCase
   alias Storex.{Sales, Store}
 
+  def book_fixture(attrs \\ %{}) do
+    default_attrs = %{
+      title: "Book1",
+      description: "Description1",
+      image_url: "book1.jpg",
+      price: "29.90"
+    }
+
+    {:ok, book} = attrs
+    |> Enum.into(default_attrs)
+    |> Store.create_book()
+
+    book
+  end
+
+  def cart_fixture() do
+    {:ok, cart} = Sales.create_cart()
+
+    cart
+  end
+
   describe "carts" do
     alias Storex.Sales.Cart
 
@@ -15,14 +36,8 @@ defmodule Storex.SalesTest do
     end
 
     test "add_book_to_cart/2 creates or increments a line_item" do
-      {:ok, book} = Store.create_book(%{
-        title: "Title",
-        description: "Description",
-        image_url: "product.jpg",
-        price: "29.90"
-        })
-
-        {:ok, cart} = Sales.create_cart()
+      book = book_fixture()
+      cart = cart_fixture()
         {:ok, line_item1} = Sales.add_book_to_cart(book, cart)
         {:ok, line_item2} = Sales.add_book_to_cart(book, cart)
 
@@ -34,14 +49,8 @@ defmodule Storex.SalesTest do
     end
 
     test "remove_book_from_cart/2 decrements or deletes a line_item" do
-      {:ok, book} = Store.create_book(%{
-        title: "Title",
-        description: "Description",
-        image_url: "product.jpg",
-        price: "29.90"
-        })
-
-        {:ok, cart} = Sales.create_cart()
+      book = book_fixture()
+      cart = cart_fixture()
         Sales.add_book_to_cart(book, cart)
         Sales.add_book_to_cart(book, cart)
 
@@ -56,31 +65,59 @@ defmodule Storex.SalesTest do
     end
 
     test "list_line_items/1 list items that belongs to a cart" do
+      book1 = book_fixture()
+      book2 = book_fixture()
+      cart1 = cart_fixture()
+      cart2 = cart_fixture()
+
+      Sales.add_book_to_cart(book1, cart1)
+      Sales.add_book_to_cart(book2, cart2)
+
+      [line_item1] = Sales.list_line_items(cart1)
+      assert line_item1.book == book1
+
+      [line_item2] = Sales.list_line_items(cart2)
+      assert line_item2.book == book2
+    end
+
+    test "line_items_quantity_count/1 returns the total quantity of items" do
+      book1 = book_fixture()
+      book2 = book_fixture()
+      cart = cart_fixture()
+
+          Sales.add_book_to_cart(book1, cart)
+          Sales.add_book_to_cart(book1, cart)
+          Sales.add_book_to_cart(book2, cart)
+
+          line_items = Sales.list_line_items(cart)
+
+          assert Sales.line_items_quantity_count(line_items) == 3
+    end
+
+    test "line_items_total_price/1 returns the total price of items" do
       {:ok, book1} = Store.create_book(%{
         title: "Title",
         description: "Description",
         image_url: "product.jpg",
-        price: "29.90"
+        price: "10.00"
         })
 
         {:ok, book2} = Store.create_book(%{
           title: "Title 2",
           description: "Description 2",
           image_url: "product.jpg",
-          price: "39.90"
+          price: "15.00"
           })
 
-          {:ok, cart1} = Sales.create_cart()
-          {:ok, cart2} = Sales.create_cart()
+          {:ok, cart} = Sales.create_cart()
 
-          Sales.add_book_to_cart(book1, cart1)
-          Sales.add_book_to_cart(book2, cart2)
+          Sales.add_book_to_cart(book1, cart)
+          Sales.add_book_to_cart(book1, cart)
+          Sales.add_book_to_cart(book2, cart)
 
-          [line_item1] = Sales.list_line_items(cart1)
-          assert line_item1.book == book1
+          line_items = Sales.list_line_items(cart)
 
-          [line_item2] = Sales.list_line_items(cart2)
-          assert line_item2.book == book2
+          assert Sales.line_items_total_price(line_items) == Decimal.new("35.00")
     end
   end
 end
